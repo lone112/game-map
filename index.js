3118dd48396b4577c4616f3cafcc5efcd7c094f4
fei.xiao <fei.xiao@yungoal.com>
2015-06-10 19:24:06 +0800

    drag limit

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var map = L.map('map',{
 	minZoom : 0,
+    maxZoom: 5,
 	zoom: 3,
 	zoomControl :false,
 	attributionControl : false
 }).setView([0,0],2);
 
-/*L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'lone112.2678b838',
-    accessToken: 'pk.eyJ1IjoibG9uZTExMiIsImEiOiI2MDkzZWU0YWQ5YmQ0MzdjNzkzM2NiMzQwY2Y2ZWU1NiJ9.3c0uj8Zw4hun9wDYRH7wtA#4/31.05/121.40'
-}).addTo(map);
-*/
+var mapBounds = new L.LatLngBounds(
+    map.unproject([0, 2560], 5),
+    map.unproject([7424, 0], 5));
+map.fitBounds(mapBounds);
+map.setMaxBounds(mapBounds);
 //http://res.cloudinary.com/lone112/image/upload/v1433932477/tiles/{z}/{x}/{y}.png.png
-L.tileLayer('http://res.cloudinary.com/lone112/image/upload/v1433932477/tiles/{z}/{x}/{y}.png.png', {
+L.tileLayer('maps/output/{z}/{x}/{y}.png', {
 		noWrap: true
 	}).addTo(map);
 
@@ -29,8 +28,3 @@ function onMapClick(e) {
 
 map.on('click', onMapClick);
 
-var mapBounds = new L.LatLngBounds(
-            map.unproject([0, 2560], mapMaxZoom),
-            map.unproject([7424, 0], mapMaxZoom));
-            
-map.fitBounds(mapBounds);
